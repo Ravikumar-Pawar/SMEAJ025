5438da90a77b5d1b5ea1268427ee5f977ea2e2ad
devipriya2919 <83400225+devipriya2919@users.noreply.github.com>
2021-06-17 07:37:02 -0700

    Add files via upload

---
+++ Collections - Devipriya/src/list/MyVector.java
@@ -0,0 +1,46 @@
+package list;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+public class MyVector {
+
+	public static void main(String[] args) {
+
+		Vector<Integer> vector = new Vector<>();
+
+		// Add elements
+		vector.add(5);
+		vector.add(4);
+		vector.add(3);
+		vector.add(2);
+		vector.add(1);
+
+		System.out.println("Vector: " + vector);
+		
+		//First Element
+		System.out.println("First Element: " + vector.firstElement());
+
+		// Contains
+		System.out.println(vector.contains(8) ? "Contains" : "Does not contain");
+		
+		//Size
+		System.out.println("Size: " + vector.size());
+		
+		//Remove by index
+		vector.remove(3);
+		System.out.println("Vector after remove by index: " + vector);
+		
+		//Remove by element
+		vector.remove(Integer.valueOf(3));
+		System.out.println("Vector after remove by object: " + vector);
+		// Using Iterator
+		Iterator<Integer> stackIterator = vector.iterator();
+		System.out.println("Using Iterator");
+		while (stackIterator.hasNext()) {
+			System.out.println(stackIterator.next());
+		}
+
+	}
+
+}
---
+++ Collections - Devipriya/src/set/MyTreeSet.java
@@ -0,0 +1,34 @@
+package set;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class MyTreeSet {
+
+	public static void main(String[] args) {
+		
+		Set<String> treeSet = new TreeSet<>();
+		
+		System.out.println("Tree Set does not allow duplicates and sorts all the elements by default");
+		
+		//Add elements
+		treeSet.add("z");
+		treeSet.add("y");
+		treeSet.add("x");
+		treeSet.add("w");
+		treeSet.add("v");
+		treeSet.add("y");
+		treeSet.add("y");
+		
+		System.out.println("Tree Set: " + treeSet);
+		
+		//Size
+		System.out.println("Size: " + treeSet.size());
+		
+		//Remove by element
+		treeSet.remove("w");
+		System.out.println("TreeSet after removal: " + treeSet);
+		
+	}
+
+}
